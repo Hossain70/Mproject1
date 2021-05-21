7ed31338caf276c5c7fbf615c46f545273fec56d
mmhos <mmhos@192.168.1.126>
2021-05-20 22:00:13 -0400

    new code added

--- src/googleFace/PullPush.java
+++ src/googleFace/PullPush.java
@@ -5,7 +5,9 @@ public class PullPush {
 	public static void main(String[] args) {
 		
 		
-		System.out.println("hellow world");
+		System.out.println("hello world");
+		
+		System.out.println("Hello  Hello HELLO");
 
 	}
 
